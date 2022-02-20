008dc632df71ffe857834717167407e3b2dd9694
DEBALINA GOSWAMI <debalina0610@gmail.com>
2022-02-20 10:51:01 +0530

    Create Java Stdin and Stdout I.java

---
+++ HackerRank/JAVA/Introduction/Java Stdin and Stdout I.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+        // Complete this line
+
+        System.out.println(a + "\n" + b + "\n" + c);
+        // Complete this line
+        // Complete this line
+    }
+}
