a0aa8c0845b5655399b008817f326e5fb5795e85
DEBALINA GOSWAMI <debalina0610@gmail.com>
2022-02-20 11:00:47 +0530

    Update If-Else.java

--- HackerRank/JAVA/Introduction/If-Else.java
+++ HackerRank/JAVA/Introduction/If-Else.java
@@ -4,21 +4,21 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner (System.in);
-    int n = sc.nextInt();
-    if(n%2 !=0)
-        System.out.println("Weird");
-    else
-    {
-        if(n%2 == 0)
+        Scanner sc = new Scanner (System.in);
+        int num = sc.nextInt();
+        if(num % 2 != 0)
+            System.out.println("Weird");
+        else
         {
-            if(n>= 2 && n<=5 )
-                System.out.println("Not Weird");
-            else if(n<=20)
-                System.out.println("Weird");
-            else
-                System.out.println("Not Weird");
+            if(num % 2 == 0)
+            {
+                if(num >= 2 && num <= 5 )
+                    System.out.println("Not Weird");
+                else if(num <= 20)
+                    System.out.println("Weird");
+                else
+                    System.out.println("Not Weird");
+            }
         }
-    }
     }
 }
