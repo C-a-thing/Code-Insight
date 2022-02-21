0757c266fb52d48af867aff6d0ceced47cb22945
SHREYA SOM <69584815+Shreya2803@users.noreply.github.com>
2022-02-22 02:30:59 +0530

    Add files via upload

---
+++ CodeChef/Maths/Guess the bottom face.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    cout<<(7-x)<<endl;
+	}
+	return 0;
+}
