44bd62ee626226a3ff69d65253a89fd784672388
DEBALINA GOSWAMI <debalina0610@gmail.com>
2022-02-22 13:04:00 +0530

    Create End of File.java

---
+++ HackerRank/JAVA/Introduction/End of File.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);  
+        int i = 0;
+        while (sc.hasNextLine()) {
+            i++;
+            System.out.println(i + " " + sc.nextLine());
+        }
+    }
+}
