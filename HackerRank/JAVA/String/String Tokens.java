4a1ca643446de56cf7dc9649c398b7511133ebdf
DEBALINA GOSWAMI <debalina0610@gmail.com>
2022-03-02 12:52:12 +0530

    Update String Tokens.java

--- HackerRank/JAVA/String/String Tokens.java
+++ HackerRank/JAVA/String/String Tokens.java
@@ -7,18 +7,18 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
-        String[] tokens = s.split("[^a-zA-Z]");
+        String[] tokens = s.split("[^a-zA-Z]");  // splitting the string into tokens
          int noTokens = 0;
         
         for (int i=0; i<tokens.length; ++i) 
             if (tokens[i].length() > 0) 
-                noTokens ++;
+                noTokens ++;  // number of tokens
              
         System.out.println(noTokens);
         
         for (int i=0; i<tokens.length;++i)
             if (tokens[i].length() > 0)
-                System.out.println(tokens[i]);
+                System.out.println(tokens[i]);  // n tokens on a new line in the same order as they appear in input string s
                 
         scan.close();
     }
